be34e993f1c0961de7e70733618f7e070e77800a
a-abhinav <abhigoyal766@gmail.com>
2023-06-16 13:22:45 +0530

    third task:adding test cases

--- battery/battery_type/spindler_battery.py
+++ battery/battery_type/spindler_battery.py
@@ -5,10 +5,9 @@ class SpindlerBattery(Battery):
     def __init__(self, current_date, last_service_date):
         self.current_date = current_date
         self.last_service_date = last_service_date
-        self.spindler_year_need_service = 4
 
     def needs_service(self):
-        date_need_service = self.last_service_date.replace(year=self.last_service_date.year + self.spinder_year_need_service)
+        date_need_service = self.last_service_date.replace(year=self.last_service_date.year + 2)
         if date_need_service < self.current_date:
             return True
         else:
